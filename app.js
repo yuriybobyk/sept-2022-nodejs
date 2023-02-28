3a82f57f3fb7a61176a7c880e88163746492bd9f
yuriybobyk <yuriybobyk4325@gmail.com>
2023-02-28 17:49:55 +0200

    done

---
+++ app.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+
+// fs.mkdir(path.join('mainFolder'), err => {
+//     if (err) throw new Error(err.message)
+// })
+
+//
+// fs.mkdir(path.join('mainFolder', 'folder1'), err => {
+//     if (err) throw new Error(err.message);
+// })
+//
+// fs.mkdir(path.join('mainFolder', 'folder2'), err => {
+//     if (err) throw new Error(err.message);
+// })
+//
+// fs.mkdir(path.join('mainFolder'), err => {
+//     if (err) throw new Error(err.message);
+// })
+//
+// fs.mkdir(path.join('mainFolder', 'folder3'), err => {
+//     if (err) throw new Error(err.message);
+// })
+//
+// fs.mkdir(path.join('mainFolder', 'folder4'), err => {
+//     if (err) throw new Error(err.message);
+// })
+//
+// fs.mkdir(path.join('mainFolder', 'folder5'), err => {
+//     if (err) throw new Error(err.message);
+// })
+//
+// fs.writeFile(path.join('mainFolder', 'file1.js'), 'File1', (err)=>{
+//     if (err) throw new Error(err.message)
+// })
+//
+// fs.writeFile(path.join('mainFolder', 'file2.js'), 'File2', (err)=>{
+//     if (err) throw new Error(err.message)
+// })
+//
+// fs.writeFile(path.join('mainFolder', 'file3.js'), 'File3', (err)=>{
+//     if (err) throw new Error(err.message)
+// })
+//
+// fs.writeFile(path.join('mainFolder', 'file4.js'), 'File4', (err)=>{
+//     if (err) throw new Error(err.message)
+// })
+//
+// fs.writeFile(path.join('mainFolder', 'file5.js'), 'File5', (err)=>{
+//     if (err) throw new Error(err.message)
+// })
+
+// fs.readdir(path.join('mainFolder'), {withFileTypes:true},(err,data)=>{
+//     if (err) throw new Error(err.message);
+//     console.log(data);
+//     data.forEach(file=>{
+//         console.log(file.isFile());
+//     })
+// })
+
+
+
+

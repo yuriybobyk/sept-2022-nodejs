913aa383cc2aeced44ecc23e16fc8bef1ef07379
yuriybobyk <yuriybobyk4325@gmail.com>
2023-03-02 21:38:29 +0200

    some changes

--- app.js
+++ app.js
@@ -5,14 +5,83 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+const users = [
+    {
+        name: 'Oleh',
+        age: 19,
+        gender: 'male'
+    },
+    {
+        name: 'Anton',
+        age: 22,
+        gender: 'female'
+    },
+    {
+        name: 'Anya',
+        age: 25,
+        gender: 'female'
+    },
+    {
+        name: 'Elizavetta',
+        age: 35,
+        gender: 'female'
+    },
+    {
+        name: 'Cocos',
+        age: 70,
+        gender: 'mixed'
+    }
+]
+
 const PORT = 5000;
 
 app.listen(PORT, ()=>{
     console.log(`Server has started well on PORT: ${PORT}`)
 })
 
-app.get('/welcome', (req, res)=>{
-console.log('WELCOME TO THE SERVER');
+app.get('/users', (req, res)=>{
+    res.json(users)
+})
 
-res.end()
+app.get('/users/:userId', (req, res)=>{
+
+    const {userId}=req.params;
+    const user = users[+userId-1];
+
+    res.json(user)
+
+})
+
+app.post('/users', (req, res)=>{
+    const body = req.body;
+    users.push(body)
+
+    res.json({
+        message: 'user created'
+    })
+})
+
+app.put('/users/:userId', (req, res)=>{
+    const {userId} = req.params;
+    const updatedUser = req.body;
+
+    users[+userId] = updatedUser;
+
+    res.json({
+        message: 'User updated',
+        data: users[+userId]
+    })
+})
+
+app.delete('/users/:userId', (req, res)=>{
+    const {userId}=req.params
+
+    users.splice(+userId, 1);
+
+    res.json({
+        message: 'user deleted'
+    })
 })

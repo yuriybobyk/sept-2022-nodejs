b5dae45e4fd5d6214208ca137ed99c3b2f9e948a
yuriybobyk <yuriybobyk4325@gmail.com>
2023-03-06 23:08:27 +0200

    lesson3 done

--- dist/app.js
+++ dist/app.js
@@ -1,30 +1,53 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        var desc = Object.getOwnPropertyDescriptor(m, k);
+        if (
+          !desc ||
+          ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)
+        ) {
+          desc = {
+            enumerable: true,
+            get: function () {
+              return m[k];
+            },
+          };
+        }
+        Object.defineProperty(o, k2, desc);
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        o[k2] = m[k];
+      });
+var __setModuleDefault =
+  (this && this.__setModuleDefault) ||
+  (Object.create
+    ? function (o, v) {
+        Object.defineProperty(o, "default", { enumerable: true, value: v });
+      }
+    : function (o, v) {
+        o["default"] = v;
+      });
+var __importStar =
+  (this && this.__importStar) ||
+  function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null)
+      for (var k in mod)
+        if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k))
+          __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose = __importStar(require("mongoose"));
@@ -33,43 +56,43 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.get("/users", async (req, res) => {
-    const users = await User_model_1.User.find();
-    res.json(users);
+  const users = await User_model_1.User.find();
+  res.json(users);
 });
 app.get("/users/:userId", async (req, res) => {
-    const { userId } = req.params;
-    const user = await User_model_1.User.findById(userId);
-    res.json(user);
+  const { userId } = req.params;
+  const user = await User_model_1.User.findById(userId);
+  res.json(user);
 });
 app.post("/users", async (req, res) => {
-    const body = req.body;
-    const user = await User_model_1.User.create({ ...body });
-    res.status(201).json({
-        message: "User created!",
-        data: user,
-    });
+  const body = req.body;
+  const user = await User_model_1.User.create({ ...body });
+  res.status(201).json({
+    message: "User created!",
+    data: user,
+  });
 });
 app.put("/users/:userId", async (req, res) => {
-    const { userId } = req.params;
-    const user = req.body;
-    const updatedUser = await User_model_1.User.updateOne({ _id: userId }, user);
-    res.status(200).json({
-        message: "User updated",
-        data: updatedUser,
-    });
+  const { userId } = req.params;
+  const user = req.body;
+  const updatedUser = await User_model_1.User.updateOne({ _id: userId }, user);
+  res.status(200).json({
+    message: "User updated",
+    data: updatedUser,
+  });
 });
 app.delete("/users/:userId", async (req, res) => {
-    const { userId } = req.params;
-    await User_model_1.User.deleteOne({ _id: userId });
-    res.status(200).json({
-        message: "User deleted",
-    });
+  const { userId } = req.params;
+  await User_model_1.User.deleteOne({ _id: userId });
+  res.status(200).json({
+    message: "User deleted",
+  });
 });
 app.get("/welcome", (req, res) => {
-    res.send("WELCOME");
+  res.send("WELCOME");
 });
 const PORT = 5100;
 app.listen(PORT, () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/sept-2022");
-    console.log(`Server has started on PORT ${PORT} 🚀🚀🚀`);
+  mongoose.connect("mongodb://127.0.0.1:27017/sept-2022");
+  console.log(`Server has started on PORT ${PORT} 🚀🚀🚀`);
 });
